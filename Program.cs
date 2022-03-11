a6a6014eb9bdcdd29ee06eb719eef6f7b17b9937
1SkYVValkeR1 <96080349+1SkYVValkeR1@users.noreply.github.com>
2022-03-11 19:33:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork.Class02.Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RealCalculator");
+
+            int firstNumber = 10;
+            int secondNumber = 15;
+
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine("theresult is:" +result);
+
+        }
+    }
+}
