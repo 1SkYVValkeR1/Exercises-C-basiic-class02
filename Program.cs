c08896c512ad6bbcb08998f05f8035a697bdc7d2
1SkYVValkeR1 <96080349+1SkYVValkeR1@users.noreply.github.com>
2022-03-11 19:34:55 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
-namespace HomeWork.Class02.Exercise01
+namespace HomeWork.Class02.Exercise02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("RealCalculator");
+            Console.WriteLine("Average Number");
 
             int firstNumber = 10;
             int secondNumber = 15;
+            int thirdNumber = 20;
+            int fourNumber = 30;
 
-            int result = firstNumber + secondNumber;
-
-            Console.WriteLine("theresult is:" +result);
-
+            int calculate = firstNumber + secondNumber + thirdNumber + fourNumber;
+            int average = calculate / 4;
+            Console.WriteLine("The average of 10, 15, 20, 30 is:" + average);
+        }
+    }
+}
         }
     }
 }
